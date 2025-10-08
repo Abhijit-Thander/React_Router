583d58900fcb723c049aa3052f37eba3522c7fce
Abhijit <abhijitthander38@gmail.com>
2025-10-08 20:23:36 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 
-function App(props) {
-  return <h1 className="text-8xl font-bold text-center">Hello World!!!</h1>;
-}
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
 
 export default App;
